ab7db90f34c96c9ae2ceb5d54ba17366f7e14a55
Jingwen Cui <jcui58@wisc.edu>
2025-10-14 18:22:40 -0500

    finish classification

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,15 +1,38 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames"
+import useGames, { Platform } from "../hooks/useGames"
 import GameCard from "./GameCard"
+import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
+import { Genre } from "../hooks/useGenres";
 
-const GameGrid = () =>{
-    const {games, error} = useGames()
+interface Props {
+    selectedGenre: Genre | null;
+    selectedPlatform: Platform | null;
+}
+
+
+const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
+    const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
+    const skeletons = [1, 2, 3, 4, 5, 6];
+
     return(
         <>
         {error && <Text>{error}</Text>}
-        <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5}} padding = '10px' spacing={10}>
-            {games.map(game => (
-                <GameCard key = {game.id} game={game}/>
+        <SimpleGrid 
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4}} 
+        padding = '10px' 
+        spacing={6}
+        >
+            {isLoading && skeletons.map((skeleton) =>(
+                <GameCardContainer key={skeleton}>
+                    <GameCardSkeleton />
+                </GameCardContainer>
+            ))} 
+            
+            {data.map(game => (
+                <GameCardContainer key = {game.id}>
+                    <GameCard game={game}/>
+                </GameCardContainer>
             ))}
         </SimpleGrid>
         </>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,40 +1,36 @@
-import { useState, useEffect } from "react";
-import apiClient from '../services/api-client'
-import { CanceledError } from "axios";
+import useData from "./useData";
+import { Genre } from "./useGenres";
+
+
+export interface Platform{
+    id: number;
+    name: string;
+    slug: string;
+}
 
 export interface Game{
     id: number;
     name: string;
     background_image: string;
-}
-
-interface FetchGamesResponse{
-    count: number;
-    results: Game[]
+    parent_platforms: { platform: Platform }[];
+    metacritic: number;
 }
 
 
-const useGames = () =>{
-
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-    
-    useEffect(()=>{
-        const controller = new AbortController();
-
-        apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal})
-        .then(res => setGames(res.data.results))
-        .catch(err => {
-            if (err instanceof CanceledError) return;
-            setError(err.message)
-        });
-            
-
-        return () => controller.abort();
-    }, []);
-
-    return { games, error};
-}
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: selectedGenre?.id,
+        platforms: selectedPlatform?.id,
+      },
+    },
+    [selectedGenre?.id, selectedPlatform?.id]
+  );
 
 
 export default useGames
